ac6008483021ea9ba646654f50bc92a90b7d75d1
Omar Pandini <mzo.pandini@gmail.com>
2018-05-19 14:00:15 -0300

    inclusão comentário

--- PDO/exemplo-04.php
+++ PDO/exemplo-04.php
@@ -2,6 +2,7 @@
 
 	$conn = new PDO("mysql:dbname=dbphp7;host=localhost","root","");
 
+	//Exemplo de delete
 	$stmt = $conn->prepare("delete from tb_usuarios where idusuario = :IDUSUARIO");
 
 	$idusuario = 2;
